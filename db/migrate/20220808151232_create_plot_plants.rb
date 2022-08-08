fd00181259d3924f764e45a0056dd96197214dcd
mkbonini <mkbonini@gmail.com>
2022-08-08 11:36:16 -0400

    fix migration plurilization

--- db/migrate/20220808151232_create_plot_plants.rb
+++ db/migrate/20220808151232_create_plot_plants.rb
@@ -1,8 +1,8 @@
 class CreatePlotPlants < ActiveRecord::Migration[5.2]
   def change
     create_table :plot_plants do |t|
-      t.references :plots, foreign_key: true
-      t.references :plants, foreign_key: true
+      t.references :plot, foreign_key: true
+      t.references :plant, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -31,12 +31,12 @@ ActiveRecord::Schema.define(version: 2022_08_08_151232) do
   end
 
   create_table "plot_plants", force: :cascade do |t|
-    t.bigint "plots_id"
-    t.bigint "plants_id"
+    t.bigint "plot_id"
+    t.bigint "plant_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["plants_id"], name: "index_plot_plants_on_plants_id"
-    t.index ["plots_id"], name: "index_plot_plants_on_plots_id"
+    t.index ["plant_id"], name: "index_plot_plants_on_plant_id"
+    t.index ["plot_id"], name: "index_plot_plants_on_plot_id"
   end
 
   create_table "plots", force: :cascade do |t|
@@ -49,7 +49,7 @@ ActiveRecord::Schema.define(version: 2022_08_08_151232) do
     t.index ["garden_id"], name: "index_plots_on_garden_id"
   end
 
-  add_foreign_key "plot_plants", "plants", column: "plants_id"
-  add_foreign_key "plot_plants", "plots", column: "plots_id"
+  add_foreign_key "plot_plants", "plants"
+  add_foreign_key "plot_plants", "plots"
   add_foreign_key "plots", "gardens"
 end

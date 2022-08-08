81a48602803bfa354b7311936ce8d211ce6497c6
mkbonini <mkbonini@gmail.com>
2022-08-08 13:52:02 -0400

    feat: order by garden plants by frequency

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -3,7 +3,9 @@ class Garden < ApplicationRecord
 
   def plant_list
     plots.joins(:plants)
-    .select("plants.*")
+    .select("plants.*, count(plants.name) as plant_count")
+    .group('plants.id')
+    .order('plant_count desc')
     .where("days_to_harvest < 100" )
     .distinct
   end

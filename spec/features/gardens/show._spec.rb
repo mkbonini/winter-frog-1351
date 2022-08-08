11dd01acfafb10aeb854900749c3dda94c74d79e
mkbonini <mkbonini@gmail.com>
2022-08-08 13:14:56 -0400

    test: garden show only has plants w/ less than 100

--- spec/features/gardens/show._spec.rb
+++ spec/features/gardens/show._spec.rb
@@ -9,11 +9,13 @@ RSpec.describe 'garden show page' do
 
         plant_1 = Plant.create!(name: 'squash', description: 'a tasty veg', days_to_harvest: 25)
         plant_2 = Plant.create!(name: 'peach', description: 'a tasty fruit', days_to_harvest: 50)
-        plant_3 = Plant.create!(name: 'carrot', description: 'an orange veg', days_to_harvest: 125)
+        plant_3 = Plant.create!(name: 'carrot', description: 'an orange veg', days_to_harvest: 30)
+        plant_4 = Plant.create!(name: 'apple', description: 'fruit from a tree', days_to_harvest: 150)
 
         PlotPlant.create!(plot: plot_1, plant: plant_1)
         PlotPlant.create!(plot: plot_1, plant: plant_2)
         PlotPlant.create!(plot: plot_2, plant: plant_1)
+        PlotPlant.create!(plot: plot_2, plant: plant_4)
 
         visit garden_path(turing_garden)
 
@@ -21,6 +23,7 @@ RSpec.describe 'garden show page' do
             expect(page).to have_content("squash",count: 1)
             expect(page).to have_content("peach",count: 1)
             expect(page).to_not have_content("carrot")
+            expect(page).to_not have_content("apple")
         end
     end
 end
